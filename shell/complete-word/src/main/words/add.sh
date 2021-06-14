11561ac9db3d68fafc84e1e894dd769f2207a198
Tom Power <tpower21@gmail.com>
2021-06-14 12:58:49 +0100

    touch added file on first run

--- shell/complete-word/src/main/words/add.sh
+++ shell/complete-word/src/main/words/add.sh
@@ -3,5 +3,8 @@ root="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 word=$1
 if [[ ! -z "$word" ]]; then
   addFile=~/.config/complete-word-autokey-rofi/words/added.txt
+  if [[ ! -d "$addFile" ]]; then
+    touch $addFile
+  fi
   grep -x $word $addFile || echo $word >> $addFile
 fi
